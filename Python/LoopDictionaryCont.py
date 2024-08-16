0435d9be1c6ec5399393b8d66d040ec3b00185ac
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-08-16 10:29:00 -0700

    exported from Skill Struck

---
+++ Python/LoopDictionaryCont.py
@@ -0,0 +1,12 @@
+ride = {
+    'Carson' : 150,
+    'Avery' : 30,
+    'Kinsley' : 40,
+    'Jackson' : 10,
+    'Dad' : 160
+}
+for x in ride.values():
+    if x >= 100:
+        print(' can ride!')
+    else:
+        print(' cannot ride...')
