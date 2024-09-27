9675d73e00a373356095c7909c67b6e086a4ec44
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:48 -0700

    exported from Skill Struck

---
+++ Python ITS Certification Prep/Math_Libary.py
@@ -0,0 +1,16 @@
+import math
+'''x = math.ceil(5.2)
+y = math.floor(5.9)
+z = math.sqrt(4)
+v = math.pow(2, 4)
+print(x)
+print(y)
+print(z)
+print(v)'''
+
+'''bikes = int(input('How many bikes are in the town'))
+people = int(input('How many people are in the town'))
+print('In this town, for every bike that exists there are ' + str(math.ceil(bikes/people)) + ' people.')'''
+
+length = int(input('Length of cube side.'))
+print(pow(length, 3))
