5d0060e62a168896c5998287c5d3637a7c765319
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:47 -0700

    exported from Skill Struck

---
+++ Python/Converting.py
@@ -0,0 +1,15 @@
+'''number = 1
+fnumber = 1.05
+strng = '1'
+floatcon = int(fnumber)
+strcon = int(strng)
+intcon = float(number)
+print(floatcon)
+print(strcon)
+print(intcon)'''
+
+'''age = input('How old are you?')
+print(int(age) + 18)'''
+
+floatnumber = float(input('Give a decimal number.'))
+print(int(floatnumber))
