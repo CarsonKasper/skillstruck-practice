49e9e0634ba7dabef3ab6fbee0d5f6fbf25fd00d
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:49 -0700

    exported from Skill Struck

---
+++ Python ITS Certification Prep/PrintingSpecifics.py
@@ -0,0 +1,10 @@
+'''print('This is \n words')
+print('This is \t words')
+print('This is \r words')'''
+
+'''print('\t\t\tSenior')
+print('\t\tJunior')
+print('\tSophomore')
+print('Freshman')'''
+
+print('Once\nupon\na\ntime')
