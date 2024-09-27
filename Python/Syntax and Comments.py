516506fcd8ff31f51103d46e9f7c7951ffd6c4ba
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:45 -0700

    exported from Skill Struck

---
+++ Python/Syntax and Comments.py
@@ -0,0 +1,7 @@
+# Here is a comment
+morning = input('What do you want to do in the morning?')
+afternoon = input('What do you want to do in the afternoon?')
+evening = input('What do you want to do in the evening?')
+print(morning)
+print(afternoon)
+print(evening)
