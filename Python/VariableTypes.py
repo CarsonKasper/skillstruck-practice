6376f0a59d949fe7276519c6345fb4677141f0a7
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:45 -0700

    exported from Skill Struck

---
+++ Python/VariableTypes.py
@@ -0,0 +1,16 @@
+'''strng = 'string'
+number = 1
+booln = True
+print(strng)
+print(number)
+print(booln)'''
+
+'''BoolT = True
+BoolF = False'''
+
+BoolT = True
+strng = "Hello!"
+number = 42
+print(BoolT)
+print(strng)
+print(number)
