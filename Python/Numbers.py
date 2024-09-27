751cbc3ed5c2c8637baf0248a74e1de75aaf44fb
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:46 -0700

    exported from Skill Struck

---
+++ Python/Numbers.py
@@ -0,0 +1,14 @@
+'''number = 1
+floatnumber = 1.57'''
+
+''''feet_in_meters = 3.28084
+print(feet_in_meters)'''
+
+hamburger = 2
+candy = 1
+movie_ticket = 4
+video_game = 7
+hamburger1 = 2.99
+candy1 = 1.99
+movie_ticket1 = 4.99
+video_game1 = 7.99
