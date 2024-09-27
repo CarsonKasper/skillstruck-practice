bfebbd4c4a6885a858c7e35b3dd13d239227039c
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:50 -0700

    exported from Skill Struck

---
+++ Python ITS Certification Prep/DifferentConcatenating .py
@@ -0,0 +1,22 @@
+'''name = 'word'
+dogs = 6
+location = 'word'
+print('My name is ' + name + ' and I have ' + str(dogs) + ' dogs and live in ' + location)
+print(f'My name is {name} and I have {dogs} dogs and live in {location}')
+print('My name is', name, 'and I have', dogs, 'dogs and live in', location)'''
+
+'''v1 = 'Word'
+v2 = 'Word'
+v3 = 'Word'
+v4 = 'Word'
+v5 = 'Word'
+'''
+v6 = 5
+v7 = 5
+v8 = 5
+v9 = 5
+v10 = 5
+'''
+print(f"{v1}{v2}{v3}{v4}{v5}{v6}{v7}{v8}{v9}{v10}")'''
+
+print('word', v6, v7, v8, v9, v10, 'word')
