4eed157536a75de3e7c7008eff312e50c5645b77
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:51 -0700

    exported from Skill Struck

---
+++ Python ITS Certification Prep/ErrorHandling.py
@@ -0,0 +1,35 @@
+# Code check
+'''v1 = 'str'
+try:
+    print(v1)
+except:
+    print('somethings wrong')
+else:
+    print('it is all good')
+finally:
+    print('Try is done')'''
+
+# Pizza party
+
+'''    slices = int(input('How many slices are there?'))
+    people = int(input('How many people are there?'))
+try:
+    number = slices/people
+except:
+    print("Your code doesn't account for if a user tries to enter 0 people")
+else:
+    print('There is no problem')
+finally:
+    print('Your exception handling is complete')
+print(number)'''
+
+# Sunflower Seeds
+Sunflowers = int(input('How many Sunflowers are there?'))
+try:
+    print(Sunflowers * 300)
+except:
+    print('There is a problem')
+else:
+    print('There is no problem')
+finally:
+    print('Your exception handling is complete')
