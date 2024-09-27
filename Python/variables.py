4d7e3b549dad9bad4afb3c755cbc15f79f52049d
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:43 -0700

    exported from Skill Struck

---
+++ Python/variables.py
@@ -0,0 +1,11 @@
+i = 1
+v = 4
+g = 5
+how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood = 5
+print(how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood)
+main_course = 'd'
+side = 'f'
+drink = 'f'
+print(main_course)
+print(side)
+print(drink)
