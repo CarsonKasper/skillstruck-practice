4a39eb3e20940bc09d8e36000cedb41bc484c7c2
CarsonKasper <121265363+CarsonKasper@users.noreply.github.com>
2024-09-27 09:26:47 -0700

    exported from Skill Struck

---
+++ Python/Concatination.py
@@ -0,0 +1,9 @@
+'''one = '1'
+two = '2'
+three = '3'
+six = one + two + three'''
+
+'''name = input('What is your name?')
+age = input('What is your age?')
+birthday = input('What is your birthday?')
+print('Hi ' + name + '! You are ' + age + ' years old and you were born on ' + birthday + '.')'''
